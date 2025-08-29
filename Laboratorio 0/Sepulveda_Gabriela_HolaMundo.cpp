49ba7e8fb83eeeff52929e63d60be670b18ef97b
ALESSA404 <sepulveda.rojas.g@gmail.com>
2025-08-28 20:33:28 -0400

    Laboratorio 0: HolaMundo, CalculoCuadrado y programa principal

---
+++ Laboratorio 0/Sepulveda_Gabriela_HolaMundo.cpp
@@ -0,0 +1,12 @@
+// Programa: Hola Mundo
+// Nombre: Gabriela Antonia Sepúlveda Rojas
+// Descripción: Este programa muestra un mensaje de saludo en la consola.
+
+#include <iostream>  // Librería para entrada y salida.
+
+int main() {
+    // Mostrar mensaje en pantalla.
+    std::cout << "Hola mundo!\n";
+
+    return 0;  // Termino del programa.
+}
